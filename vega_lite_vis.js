644911583a92b9f5f0ccfc4a2d5295ee42f606fe
Srihitha-26 <140134803+Srihitha-26@users.noreply.github.com>
2023-10-02 21:59:17 +0800

    js file

---
+++ vega_lite_vis.js
@@ -0,0 +1,7 @@
+var vg_1 = "suicide_rate_map.vg.json"
+
+vegaEmbed("#map_chart", vg_1).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    
+}).catch(console.error);
